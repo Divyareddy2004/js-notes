af79ad1e1d11ae2926ca0d912ae257e716c68009
Divyareddy2004 <164653480+Divyareddy2004@users.noreply.github.com>
2026-05-19 10:12:35 +0530

    Task-2

---
+++ script.js
@@ -0,0 +1,175 @@
+1//Create a variable and store your favorite food.
+    var favoritefood = "Biryani"
+
+// 2. Create a variable and store your mobile number.
+    var mobilenumber = "987654321"
+
+//Create a boolean variable with false value.
+    let c = (false)
+    console.log(c);
+
+// 4. Create one undefined variable and print typeof.
+    let x;
+    console.log(typeof(x));
+
+// 5. Create one array with 6 animal names.
+    let animal =["Lion","elephant","Tiger","Monkey","Dog","cat"]
+
+// 6. Print second animal from array.
+    let animal =["Lion","elephant","Tiger","Monkey","Dog","cat"]
+    console.log(animal[1]);
+
+// 7. Print last animal from array using length.
+    let animal =["Lion","elephant","Tiger","Monkey","Dog","cat"]
+    console.log(animal[animal.length[-1]]);
+
+//8.Create object with bike details (name, model, color).
+    let bike = {
+        name : "Royal enfield"
+        model : "classic 350"
+        color : "Black"
+    };
+
+// 9. Print bike color from object.
+    console.log(bike.["color"]);
+
+// 10. Create two variables and subtract values.
+    let x = 10
+    let y= 5
+    console.log(x-y);
+
+// 11. Create two variables and divide values.
+    let x=10
+    let y=5
+    console.log(x/y);
+
+// 12. Find 15 % 2.
+    console.log(15%2);
+    // remainder=1
+
+// 13. Find 2 ** 5.
+    console.log(2**5);
+    //remainder = 32
+
+// 14. Create variable with 5 and use post increment.
+    let x=5
+    x++
+    console.log(x);
+    //remainder = 6
+
+// 15. Create variable with 10 and use pre increment.
+    let x=10
+    ++x
+    console.log(x);
+    //remainder = 11
+
+// 16. Create variable with 20 and use post decrement.
+    let x=20
+    x--
+    console.log(x);
+    //remainder = 19
+
+// 17. Create variable with 50 and use pre decrement.
+    let x=50
+    --x
+    console.log(X);
+    //remainder = 49
+
+// 18. Check 100 > 50.
+    True
+
+// 19. Check 25 < 10.
+    False
+
+// 20. Check 50 >= 50.
+    True
+
+// 21. Check 30 <= 25.
+    False
+
+// 22. Check 10 == "10".
+    True 
+
+// 23. Check 10 === "10".
+    False 
+
+// 24. Check 25 != "25".
+    False 
+
+// 25. Check 25 !== "25".
+    True 
+
+// 26. Use AND operator:
+//     10 > 5 && 20 > 15
+    console.log(10>5 && 20>15);
+    True
+
+// 27. Use AND operator:
+//     5 > 10 && 20 > 15
+    False
+
+// 28. Use OR operator:
+//     5 > 10 || 20 > 15
+    True
+
+// 29. Use OR operator:
+//     2 > 5 || 1 > 10
+    False
+
+// 30. Use NOT operator:
+//     !(10 > 5)
+    False
+// 31. Use NOT operator:
+//     !(5 > 10)
+    True
+
+// 32. Create ternary operator:
+//     if 50 > 25 print "True"
+//     else print "False"
+    let x=50>25? "True": "False";
+    console.log(x);
+    True
+
+// 33. Create ternary operator:
+//     if 18 >= 21 print "Adult"
+//     else print "Minor"
+    let y= 18 >= 21? "Adult" : "Minor"
+    console.log(y);
+    Minor 
+
+//34. Create one array with 5 city names and print third city.
+    let cities = ["BEngaluru","hyderabad","Chennai","Mumbai","Kerala"]
+    console.log(cities[2]);
+
+// 35. Create one object with employee details and print employee name.
+    let employee={
+        name : " Divyagna"
+        id : "123"
+        department : "IT"
+    };
+    console.log(employee.name);
+    
+// 36. Create variable with string "100" and print typeof.
+    let strnum ="10";
+    console.log(typeof(strnum));
+
+// 37. Create variable with number 100 and print typeof.
+    let num = "20"
+    console.log(typeof(num));
+
+// 38. Create one null variable and print it.
+    let emptyvalue = null;
+    console.log(typeof(emptyvalue));
+
+// 39. Create one array with mixed datatypes.
+    let mixedarray=["Divyagna","22","true","null","50.05"]
+    console.log(mixedarray);
+
+// 40. Create object with student details:
+//     name, mark, passed(boolean)
+let student={
+    name : "Divyagna"
+    mark : "90"
+    passed : "true"
+};
+console.log(student);
